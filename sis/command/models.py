9976d6f7cc54b04045b67219413ecb70e7d866f5
CoderEx24 <coderex24@protonmail.com>
2024-04-22 22:33:22 +0200

    backend: implmemented simple models

--- sis/command/models.py
+++ sis/command/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+SEMESTER_TYPE = (
+        (1, 'Summer'),
+        (2, 'Fall'),
+        (3, 'Spring'),
+)
+
+class Instructor(models.Model):
+    userinfo = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='instructor')
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=250)
+
+class Major(models.Model):
+    faculty = models.OneToOneField(Faculty, on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    degree = models.CharField(max_length=250)
+
+class Course(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    credit_hours = models.IntegerField()
+    mandatory = models.BooleanField()
+    faculty = models.OneToOneField(Faculty, on_delete=models.CASCADE)
+
+class Semester(models.Model):
+    kind = models.TextField(choices=SEMESTER_TYPE)
+    year = models.DateField()
+
+
--- sis/student/models.py
+++ sis/student/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    userinfo = models.OneToOneField('auth.User', on_delete=models.CASCADE, related_name='student')
+    cgpa = models.FloatField()
+    major = models.OneToOneField('command.Major', on_delete=models.SET_NULL, null=True)
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey('command.Course', on_delete=models.CASCADE)
+    semester = models.ForeignKey('command.Semester', on_delete=models.CASCADE)
+    instructor = models.ForeignKey('command.Instructor', on_delete=models.CASCADE)
+    classwork_marks = models.IntegerField()
+    homework_marks = models.IntegerField()
+    midterm_mark = models.IntegerField()
+    final_mark = models.IntegerField()
+    gpa = models.FloatField()
+
